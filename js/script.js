96fce16818f1dda175a77b00ddbd2d44e90a86d5
dbonafin <demianbonafina@gmail.com>
2022-06-16 14:57:26 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Visualizzare in un alert 5 numeri casuali.
+// 30 secondi dopo la chiusura dell'alert, l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
